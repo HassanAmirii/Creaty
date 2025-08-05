d851aca21e60ab1e4c781cb655f0edf7144270d1
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-08-05 14:35:43 +0100

    Fix task input handling and update HTML structure for better task display

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const messageBox = document.getElementById("messageBox");
+  const taskBox = document.getElementById("taskBox");
   document.addEventListener("submit", (e) => {
-    const todoInput = document.getElementById("todoInput").value();
+    e.preventDefault();
+    const todoInput = document.getElementById("todoInput").value;
     if (todoInput) {
       localStorage.setItem("newTask", todoInput);
+
+      const getTask = localStorage.getItem("newTask");
+      console.log(getTask);
+      taskBox.innerHTML = `<p> <input type="checkbox" name="" id=""> ${getTask}</p>`;
     } else {
+      messageBox.innerHTML = `<p> Please input a task in the box above</p>
+`;
     }
   });
 });
