3a85c959358df1ecf88258b028557023d8374832
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-08-08 21:32:50 +0100

    Refactor task submission and rendering logic for improved clarity and functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,34 +7,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // show available task
   renderTask();
-
   // listen and render new  task submission
-  function renderTask() {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const todoInput = document.getElementById("todoInput").value;
 
-      if (todoInput) {
-        // retrieve old task if available 0r create a new one
-        const storedTask = localStorage.getItem("newTask");
-        let taskList = storedTask ? JSON.parse(storedTask) : [];
-        taskList.push({ task: todoInput });
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let todoInput = document.getElementById("todoInput").value;
 
-        localStorage.setItem("newTask", JSON.stringify(taskList));
-        const getTask = JSON.parse(localStorage.getItem("newTask"));
-
-        // manipulate the above objects list, add check box, delete button, and arrange each object as a list
-        const arrangeGetTask = getTask
-          .map(function (taskItem) {
-            return `<p><input id="taskCheckBox" type="checkbox"> ${taskItem.task} <button id="deleteTask">delete</button></p>`;
-          })
-          .join("");
-        // render task on screen
-        taskBox.innerHTML = arrangeGetTask;
-      } else {
-        messageBox.innerHTML = `<p> Please input a task in the box above</p>
+    if (todoInput) {
+      // retrieve old task if available 0r create a new one
+      renderTask(todoInput);
+      document.getElementById("todoInput").value = "";
+    } else {
+      messageBox.innerHTML = `<p> Please input a task in the box above</p>
 `;
-      }
-    });
+    }
+  });
+  function renderTask(taskList) {
+    const storedTask = localStorage.getItem("newTask");
+    let taskList = storedTask ? JSON.parse(storedTask) : [];
+    taskList.push({ task: todoInput });
+
+    localStorage.setItem("newTask", JSON.stringify(taskList));
+    const getTask = JSON.parse(localStorage.getItem("newTask"));
+
+    // manipulate the above objects list, add check box, delete button, and arrange each object as a list
+    const arrangeGetTask = getTask
+      .map(function (taskItem) {
+        return `<p><input id="taskCheckBox" type="checkbox"> ${taskItem.task} <button id="deleteTask">delete</button></p>`;
+      })
+      .join("");
+    // render task on screen
+    taskBox.innerHTML = arrangeGetTask;
   }
 });
