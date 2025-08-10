007b953e04316c87e937f1a2aac4f2475267040e
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-08-10 10:50:34 +0100

    Fix task count element ID and improve task completion counter logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const messageBox = document.getElementById("messageBox");
   const taskBox = document.getElementById("taskBox");
   const form = document.getElementById("form");
-  const taskCount = document.getElementById("taskCount");
+  const taskCountBox = document.getElementById("taskCountBox");
 
   // show available task
   renderTask();
@@ -31,46 +31,43 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // Get task from storage manipulate all and show on screen
+  // show available task in storage
   function renderTask() {
     const getTask = JSON.parse(localStorage.getItem("newTask"));
-    console.log(getTask);
-    const arrangeGetTask = getTask
+    const CleanUpTask = getTask
       .map(function (taskItem, index) {
-        return `<p><input class="CheckState" data-index =${index} type="checkbox"> ${taskItem.task} <button data-index =${index} class="deleteBtn">delete</button></p>`;
+        return `<p><input class="checkBOX" data-index =${index} type="checkbox"> ${taskItem.task} <button data-index =${index} class="deleteBtn">delete</button></p>`;
       })
       .join("");
-    taskBox.innerHTML = arrangeGetTask;
+    taskBox.innerHTML = CleanUpTask;
+
     messageBox.innerHTML = "";
-
-    // delete a task
-    const allTask = document.querySelectorAll(".deleteBtn");
-    allTask.forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        deleteTask(Number(btn.dataset.index));
-      });
-    });
-
-    // implementing task completion counter
-    const CheckState = document.querySelectorAll(".CheckState");
-    CheckState.forEach(function (checkItem) {
-      checkItem.addEventListener("change", (e) => {
-        let taskCounter = 0;
-        CheckState.forEach((checkItem) => {
-          if (checkItem.checked) taskCounter++;
-        });
-        taskCount.innerHTML = `${taskCounter} / ${CheckState.length} tasks completed`;
-      });
-    });
   }
 
   // Get task from storage > delete selected one >  show the rest on screen
-
-  function deleteTask(index) {
-    const fromStorage = localStorage.getItem("newTask");
-    let taskList = fromStorage ? JSON.parse(fromStorage) : [];
-    taskList.splice(index, 1);
-    localStorage.setItem("newTask", JSON.stringify(taskList));
-    renderTask();
+  function deleteTask() {
+    const allTask = document.querySelectorAll(".deleteBtn");
+    allTask.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        const index = btn.dataset.index;
+        const fromStorage = localStorage.getItem("newTask");
+        let taskList = fromStorage ? JSON.parse(fromStorage) : [];
+        taskList.splice(index, 1);
+        localStorage.setItem("newTask", JSON.stringify(taskList));
+      });
+    });
   }
+  deleteTask();
+
+  function CountCompletedTaskAndRender() {
+    let taskCounter = 0;
+    const checkBOX = document.querySelectorAll(".checkBOX");
+    checkBOX.forEach(function (checkBoxItem) {
+      checkBoxItem.addEventListener("change", () => {
+        if (checkBoxItem.checked) taskCounter++;
+      });
+    });
+    taskCountBox.innerHTML = `<p> ${taskCounter} / ${checkBOX.length} completed</p>`;
+  }
+  CountCompletedTaskAndRender();
 });
